81c28f82b47e17e0060aca43b51a498a4fbc8fa2
Otavio-Novais <otavioaugustonv@gmail.com>
2024-03-30 17:58:30 -0300

    Criação da primeira base do código

--- principal/src/App.java
+++ principal/src/App.java
@@ -1,8 +1,38 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    //Code
+
 }
 
-//This is a test
-// Another test to see gi
+class Contacts{
+    private int idContacts;
+    private String name;
+    private String job;
+    private String phone;
+    private String email;
+    private String company;
+
+    private Contacts(int id, String n, String j, String p, String e, String c){
+        this.idContacts = id;
+        this.name = n;
+        this.job = j;
+        this.phone = p;
+        this.email = e;
+        this.company = c;
+    }
+
+    public void addContact(){
+//Função 
+    }
+
+    public void updateContact(){
+//Função
+    }
+
+    public void deleteContact(){
+//Função
+    }
+
+    public void showContact(){
+//Função
+    }
+}
