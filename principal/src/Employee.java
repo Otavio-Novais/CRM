046cd1e07940162d83e40e79a29d714e455cfeef
Victor Augusto <victoraugusto2812@gmail.com>
2024-04-01 21:36:35 -0300

    "Adicionando Employee"

---
+++ principal/src/Employee.java
@@ -0,0 +1,9 @@
+// Principio de Segregação de Interface
+public interface Employee {
+    public void addEmployee(String n, String j, String p,Double k, Double i);
+
+    public void updateEmployee(String n, String j, String p, Double k, Double i);
+
+    public void deleteEmployee();
+    public void showEmployee();
+}
